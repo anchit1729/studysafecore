06e033d30d82283c574a8804e8732db410768161
Anchit Mishra <champu.1729@icloud.com>
2022-05-04 15:02:31 +0530

    Deploy Core

--- core/settings.py
+++ core/settings.py
@@ -29,11 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str('QDD_SECRET_KEY')
-#SECRET_KEY = 'django-insecure-@#$py-*@4$aif8gpka2!uq14(1a+aq!msg!wnk&jf#q+vweznj'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = env.str('QDD_DEBUG', default = False)
 
 ALLOWED_HOSTS = ['cryptic-tor-77631.herokuapp.com', 'localhost', '127.0.0.1']
